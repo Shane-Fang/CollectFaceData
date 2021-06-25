edc4a1e9e90b4dede79db3d9356dc7e7a8e34da8
劭勳 枋 <shane.fang@cypress.com.tw>
2021-06-25 17:45:57 +0800

    20210625

--- CV.py
+++ CV.py
@@ -1,51 +1,55 @@
 import cv2
 import os
 
+'''
+    控制OpenCV的類別，包括讀取鏡頭和儲存影像等等
+'''
 
 class CV:
     def __init__(self):
         self.cv = cv2
-        self.camera = self.cv.VideoCapture(cv2.CAP_DSHOW)
-        self.classfier = self.cv.CascadeClassifier("haarcascade_frontalface_default.xml")
-        self.frame = None
-        self.color = (0, 255, 0)
-        self.person_name = ''
-        self.num = 0
-        self.Collect = False
-        self.dirPath = ''
+        self.camera = self.cv.VideoCapture(cv2.CAP_DSHOW)  # 設定影像來源為電腦鏡頭
+        self.classfier = self.cv.CascadeClassifier("haarcascade_frontalface_default.xml")  # 載入辨識人臉工具
+        self.frame = None  # 揁畫面
+        self.color = (0, 255, 0)  # 邊框顏色
+        self.person_name = ''  # 收集資料者的姓名
+        self.num = 0  # count收集張數
+        self.Collect = False  # 開啟收集功能
+        self.dirPath = ''  # 存放影像路徑
 
-    def read(self):
+    def read(self):  # 讀取鏡頭
         return self.camera.read()
 
-    def getImg(self, frame):
+    def getImg(self, frame):  # 獲取鏡頭影像轉成圖片，顯示在UI上或儲存成圖片檔
         self.frame = frame
-        grey = self.cv.cvtColor(self.frame, self.cv.COLOR_BGR2GRAY)
-        faceRects = self.classfier.detectMultiScale(grey, scaleFactor=1.2, minNeighbors=5, minSize=(32, 32))
+        grey = self.cv.cvtColor(self.frame, self.cv.COLOR_BGR2GRAY)  # 影像RGB轉Gray
+        faceRects = self.classfier.detectMultiScale(grey, scaleFactor=1.2, minNeighbors=5, minSize=(32, 32))  # 識別人臉
 
-        if len(faceRects) > 0:
+        if len(faceRects) > 0:  # 如果識別到人臉，則執行以下功能
             for faceRect in faceRects:
-                x, y, w, h = faceRect
-                if self.Collect:
-                    if os.path.isdir('%s/%s' % (self.dirPath, self.person_name)):
-                        img_name = '%s/%s/%04d.jpg' % (self.dirPath, self.person_name, self.num)
-                        image = self.frame[y - 10: y + h + 10, x - 10: x + w + 10]
-                        if image.size == 0:
-                            print('%s have problem' % self.num)
+                x, y, w, h = faceRect  # 取得座標及長寬
+                if self.Collect:  # 是否在收集資料
+                    if os.path.isdir('%s/%s' % (self.dirPath, self.person_name)):  # 判斷是否有這路徑，沒有就創建
+                        img_name = '%s/%s/%04d.jpg' % (self.dirPath, self.person_name, self.num)  # 圖片檔名
+                        image = self.frame[y - 10: y + h + 10, x - 10: x + w + 10]  # 稍微放大一點
+                        if image.size == 0:  # 如果上面的稍微放大一點的計算出現負數，圖片會沒有長寬，所以在這邊做個判斷
+                            print('%s has problem' % self.num)
                         else:
-                            self.num += 1
-                            self.cv.imwrite(img_name, image)
+                            self.num += 1  # 計數收集張數
+                            self.cv.imwrite(img_name, image)  # 寫入圖檔
                         print(self.num)
                     else:
-                        os.mkdir('%s/%s' % (self.dirPath, self.person_name))
+                        os.mkdir('%s/%s' % (self.dirPath, self.person_name))  # 創建路徑
 
-                self.cv.rectangle(self.frame, (x - 10, y - 10), (x + w + 10, y + h + 10), self.color, 2)
+                self.cv.rectangle(self.frame, (x - 10, y - 10), (x + w + 10, y + h + 10), self.color, 2)  # 畫框框
 
-        return self.cv.cvtColor(self.frame, cv2.COLOR_BGR2RGBA)
+        return self.cv.cvtColor(self.frame, cv2.COLOR_BGR2RGBA)  # 返回影像
 
-    def CollectData(self, person_name: str):
+    def CollectData(self, person_name: str):  # 收集圖片
         self.person_name = person_name
         self.Collect = True
 
-    def close(self):
+    def close(self):  # 關閉影像串流
+
         self.camera.release()
         self.cv.destroyAllWindows()
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from CV import CV
 from sys import exit
 
 
-def Descirption():
+def Descirption():  # 顯示使用說明視窗
     t = Tk()
     t.title("收集圖片")
     t.geometry('600x400')
@@ -18,7 +18,7 @@ def Descirption():
     descirption.mainloop()
 
 
-def check_contain_chinese(check_str):
+def check_contain_chinese(check_str):  # 檢查名字或路徑是否有中文或空格，返回Boolean
     for ch in check_str:
         if u'\u4e00' <= ch <= u'\u9fff':
             print(ch)
@@ -30,70 +30,70 @@ def check_contain_chinese(check_str):
     return False
 
 
-def Warning(txt: str):
+def Warning(txt: str):  # 顯示警告視窗
     messagebox.showinfo('警告', txt)
 
 
-def start():
-    if not name.get() or len(name.get()) == 0:
+def start():  # 點擊開始按鈕事件
+    if not name.get() or len(name.get()) == 0:  # 檢查名字輸入框是否為空
         Warning('請輸入英文名字')
         text.set('請輸入英文名字')
         return
 
-    if not cv.dirPath or len(cv.dirPath) == 0:
+    if not cv.dirPath or len(cv.dirPath) == 0:  # 檢查路徑是否為空
         Warning('請選擇路徑')
         text.set('請選擇路徑')
         return
 
-    if not name.get().isalpha():
+    if not name.get().isalpha():  # 檢查名字是否都是英文
+        name.delete(0, END)  # 清空名字輸入格
+        Warning('名字內不可有中文、符號或空格')
+        text.set('名字內不可有中文、符號或空格')
+        return
+
+    if check_contain_chinese(name.get()):  # 檢查名字是否有中文或空格，返回Boolean
         name.delete(0, END)
         Warning('名字內不可有中文、符號或空格')
         text.set('名字內不可有中文、符號或空格')
         return
 
-    if check_contain_chinese(name.get()):
-        name.delete(0, END)
-        Warning('名字內不可有中文、符號或空格')
-        text.set('名字內不可有中文、符號或空格')
-        return
-
-    if check_contain_chinese(cv.dirPath):
+    if check_contain_chinese(cv.dirPath):  # 檢查路徑是否有中文或空格，返回Boolean
         print(cv.dirPath)
         cv.dirPath = ''
         Warning('路徑內不可有中文或空格')
         text.set('路徑內不可有中文或空格')
         return
 
-    setPath['state'] = DISABLED
+    setPath['state'] = DISABLED  # 設定按鈕為不可使用
     start_btn['state'] = DISABLED
     name['state'] = DISABLED
 
-    cv.CollectData(name.get())
+    cv.CollectData(name.get())  # 如果名字和路徑正確，就開始收集資料
 
 
-def end():
+def end():  # 點擊結束按鈕事件
     exit()
 
 
-def selectPath():
-    path = filedialog.askdirectory()
-    if not path or len(path) == 0:
+def selectPath():  # 點擊選擇路徑按鈕事件
+    path = filedialog.askdirectory()  # 路徑選擇瀏覽器
+    if not path or len(path) == 0:  # 檢查路徑是否為空
         text.set('請選擇路徑')
     else:
         cv.dirPath = path
         text.set('以選擇路徑:%s' % path)
 
 
-def video_loop():
-    success, img = cv.read()
-    if success:
+def video_loop():  # 顯示鏡頭畫面的功能
+    success, img = cv.read()  # 讀取鏡頭影像
+    if success:  # 如果成功讀取
         cv2.waitKey(10)
-        cv2image = cv.getImg(img)
+        cv2image = cv.getImg(img)  # 獲取影像圖片
 
-        if cv.Collect:
+        if cv.Collect:  # 如果正在收集資料，則顯示收集進度
             text.set('已收集:%s/1000張' % cv.num)
 
-        if cv.num == 1000:
+        if cv.num == 1000:  # 如果收集完1000張，則結束收集
             cv.num = 0
             cv.Collect = False
             cv.person_name = None
@@ -101,7 +101,7 @@ def video_loop():
             text.set('收集完畢')
 
             t = Tk()
-            t.title("你的帥氣或美麗臉龐收集完成")
+            t.title("你的帥氣或美麗臉龐收集完成")  # 跳出結束提醒視窗
             t.geometry('500x200')
             t.resizable(width=0, height=0)
             descirption = Label(t, text="\n\n請把儲存圖片的資料夾壓縮傳至\n\\\\192.168.5.5\Public\RD_11\劭勳\data",
@@ -110,55 +110,55 @@ def video_loop():
             descirption.mainloop()
 
         current_image = Image.fromarray(cv2image)
-        imgtk = ImageTk.PhotoImage(image=current_image)
+        imgtk = ImageTk.PhotoImage(image=current_image)  # 用ImageTK將圖片顯示在label物件上
         panel.imgtk = imgtk
         panel.config(image=imgtk)
-        window.after(10, video_loop)
+        window.after(10, video_loop)  # UI迴圈，每10毫秒執行video_loop
 
 
 cv = CV()
 
-window = Tk(className='人臉影像收集')
-window.title("收集圖片")
-window.geometry('900x550')
-window.resizable(width=0, height=0)
+window = Tk(className='人臉影像收集')  # 建立UI
+window.title("收集圖片")  # title
+window.geometry('900x550')  # 大小
+window.resizable(width=0, height=0)  # 設定不能放大縮小
 
-text = StringVar()
-text.set('輸入你的英文名字和選擇圖片儲存路徑')
+text = StringVar()  # Tkinter文字，可以放進各種能顯示文字的物件上
+text.set('輸入你的英文名字和選擇圖片儲存路徑')  # 設定文字
 
-descirption = Button(window, text="說明", command=Descirption)
-descirption['width'] = 20
+descirption = Button(window, text="說明", command=Descirption)  # 說明按鈕
+descirption['width'] = 20  # 設定長寬
 descirption['height'] = 3
-descirption.place(x=700, y=50)
+descirption.place(x=700, y=50)  # 設定位置座標，左上角為(0, 0)
 
-panel = Label(window)
-panel.place(x=0, y=50)
-window.config(cursor="arrow")
+panel = Label(window)  # label物件，顯示影像用
+panel.place(x=0, y=50)  # 設定位置座標
 
-name = Entry(window, width=15, bd=6, font='Arial 20')
-name.place(x=650, y=150)
+name = Entry(window, width=15, bd=6, font='Arial 20')  # 文字輸入框
+name.place(x=650, y=150)  # 設定位置座標
 
-label = Label(window, font="標楷體 16", textvariable=text, anchor=NW, wraplength=900)
-label['width'] = 80
+label = Label(window, font="標楷體 16", textvariable=text, anchor=NW, wraplength=900)  # label物件，顯示上方文字提示
+label['width'] = 80  # 設定長寬
 label['height'] = 2
-label.place(x=0, y=0)
+label.place(x=0, y=0)  # 設定位置座標
 
-setPath = Button(window, text="選擇路徑", command=selectPath)
-setPath['width'] = 20
+setPath = Button(window, text="選擇路徑", command=selectPath)  # 選擇路徑按鈕，command為上方的selectPath()
+setPath['width'] = 20  # 設定長寬
 setPath['height'] = 3
-setPath.place(x=700, y=275)
+setPath.place(x=700, y=275)  # 設定位置座標
 
-start_btn = Button(window, text="開始", command=start)
-start_btn['width'] = 20
+start_btn = Button(window, text="開始", command=start)  # 開始按鈕
+start_btn['width'] = 20  # 設定長寬
 start_btn['height'] = 3
-start_btn.place(x=700, y=350)
+start_btn.place(x=700, y=350)  # 設定位置座標
 
-end_btn = Button(window, text="結束", command=end)
-end_btn['width'] = 20
+end_btn = Button(window, text="結束", command=end)  # 結束按鈕
+end_btn['width'] = 20  # 設定長寬
 end_btn['height'] = 3
-end_btn.place(x=700, y=425)
+end_btn.place(x=700, y=425)  # 設定位置座標
 
 video_loop()
 
-window.mainloop()
-cv.close()
+window.mainloop()  # UI 線程
+cv.close()  # 關閉影像串流
+
